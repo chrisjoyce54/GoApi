46d4f9b72fc9c16115863a6efa7f024e535fb625
chris <chris@joycenet.us>
2024-05-07 10:24:06 -0500

    Improved middleware #189

--- routes/routes.go
+++ routes/routes.go
@@ -6,11 +6,16 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/events", getEvents)    // GET, POST, PUT, PATCH, DELETE
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+
+	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent) // /events/1, /events/5
-	server.POST("/events", middlewares.Authenticate, createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
